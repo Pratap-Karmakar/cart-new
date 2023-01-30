f2ea8e589d57f6ba3570f5e6657b1b06762bb4d6
Pratap-Karmakar <pratap2501.pk@gmail.com>
2023-01-30 20:00:39 +0530

    Added total price

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 
 
 // class based component
-class App extends React.Component{
+class App extends React.Component {
 
   // to add state
   constructor() {
@@ -12,29 +12,29 @@ class App extends React.Component{
     super();
     // state is the way to store the local data for that particular component, and state is a javascript object
     this.state = {
-        products: [
-            {
-                price: "999",
-                title: 'Watch',
-                qty: 1,
-                img: 'https://images.unsplash.com/photo-1523170335258-f5ed11844a49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
-                id: 1
-            },
-            {
-                price: "9,999",
-                title: 'Phone',
-                qty: 10,
-                img: 'https://images.unsplash.com/photo-1567581935884-3349723552ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
-                id: 2
-            },
-            {
-                price: "49,999",
-                title: 'Laptop',
-                qty: 4,
-                img: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
-                id: 3
-            }
-        ]
+      products: [
+        {
+          price: 999,
+          title: 'Watch',
+          qty: 1,
+          img: 'https://images.unsplash.com/photo-1523170335258-f5ed11844a49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80',
+          id: 1
+        },
+        {
+          price: 9999,
+          title: 'Phone',
+          qty: 10,
+          img: 'https://images.unsplash.com/photo-1567581935884-3349723552ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+          id: 2
+        },
+        {
+          price: 49999,
+          title: 'Laptop',
+          qty: 4,
+          img: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80',
+          id: 3
+        }
+      ]
     }
   }
 
@@ -47,7 +47,7 @@ class App extends React.Component{
 
     products[index].qty += 1;
     this.setState({
-        products
+      products
     })
   }
 
@@ -59,12 +59,12 @@ class App extends React.Component{
 
     // if the qty is already zero then it can't be go negative that's why
     if (products[index].qty === 0) {
-        return;
+      return;
     }
 
     products[index].qty -= 1;
     this.setState({
-        products
+      products
     })
   }
 
@@ -75,28 +75,44 @@ class App extends React.Component{
     const items = products.filter((item) => item.id !== id);
 
     this.setState({
-        products: items
+      products: items
     })
   }
 
-// Navbar icon count functiongetCartCount 
-getCartCount = () => {
-  const {products}= this.state;
-  let count = 0;
-  products.forEach(product => {
-    count += product.qty
-  });
-  return count;
-}
+  // Navbar icon count functiongetCartCount 
+  getCartCount = () => {
+    const { products } = this.state;
+    let count = 0;
+    products.forEach(product => {
+      count += product.qty
+    });
+    return count;
+  }
 
+  // to get the total price of the cart function
+  getCartTotal = () => {
+    const { products } = this.state;
 
-render () {
-  const {products} = this.state;
+    let cartTotal = 0;
+
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price
+    })
+
+    return cartTotal;
+  }
+
+  render() {
+    const { products } = this.state;
     return (
       <div className="App">
         {/* to add the count in the cart icon on the Navbar */}
-        <Navbar count={this.getCartCount()}/>
-        <Cart products={products} onIncreaseQuantity={this.handelIncreaseQuantity} onDecreaseQuantity={this.handelDecreaseQuantity} onDeleteProduct={this.handleDeleteProduct}/>
+        <Navbar count={this.getCartCount()} />
+        <Cart products={products} onIncreaseQuantity={this.handelIncreaseQuantity} onDecreaseQuantity={this.handelDecreaseQuantity} onDeleteProduct={this.handleDeleteProduct} />
+
+        <div style={{fontSize: 30, padding: 10}}>
+          Total: {this.getCartTotal()}
+        </div>
       </div>
     );
   }
